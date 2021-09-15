9d73a960f8642782f1175cbdb57027a737de1217
Elvis de Andrade Santos <elvisdeandrade@outlook.com>
2021-09-15 20:13:41 -0300

    Back button LoginActivity click listener

--- frontend/mobile/android/app/src/main/java/com/evergreen/app/LoginActivity.kt
+++ frontend/mobile/android/app/src/main/java/com/evergreen/app/LoginActivity.kt
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Window
 import android.view.Window.*
+import android.content.Intent
+import android.widget.ImageView
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,5 +13,13 @@ class LoginActivity : AppCompatActivity() {
         this.requestWindowFeature(FEATURE_NO_TITLE)
         supportActionBar?.hide()
         setContentView(R.layout.activity_login)
+
+        val loginBackButton = findViewById<ImageView>(R.id.loginBackButton)
+
+        loginBackButton.setOnClickListener{
+            val intent = Intent(this,MainActivity::class.java)
+
+            startActivity(intent)
+        }
     }
 }
